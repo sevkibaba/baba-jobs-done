43d743f8448023d34845f90ecf7e81cba75b7f82
sevkibaba <sevkiyontem@gmail.com>
2016-11-20 17:38:46 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ var fs = require('fs');
 var random = require('unique-random')(1, 100);
 
 var date = new Date();
-var data = process.argv[2] + ' ' + date;
 
-var numbers = [random(),random(),random(),random()];
+if (process.argv[2]) {
+var mydata = process.argv[2] + ' ' + date;
+writeToFile(mydata);
+}
 
-fs.appendFile('jobsDone.txt',data , function (err) {
+function writeToFile (data) {
+	fs.appendFile('jobsDone.txt',data , function (err) {
   if (err) throw err;
-  console.log('Work harder bitch!');
-  console.log('Your luck number is: ' + random() + 
-  	' Use it wisely!\r\n');
+  
 });
+}
+
+module.exports = {
+	writeToFile: writeToFile
+};
+
+
+// fs.appendFile('jobsDone.txt',data , function (err) {
+//   if (err) throw err;
+//   console.log('Work harder bitch!');
+//   console.log('Your luck number is: ' + random() + 
+//   	' Use it wisely!\r\n');
+// });
